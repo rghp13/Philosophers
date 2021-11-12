45c0a66ab84956f4e3a85e7d36515046cf82c10c
Romain PONSONNET <rponsonn@e1r4p4.42.fr>
2021-11-12 16:06:49 +0100

    more bugfixing, out of order locking

--- philo/print.c
+++ philo/print.c
@@ -6,7 +6,7 @@
 /*   By: rponsonn <rponsonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 16:49:30 by rponsonn          #+#    #+#             */
-/*   Updated: 2021/11/12 14:09:49 by rponsonn         ###   ########.fr       */
+/*   Updated: 2021/11/12 14:49:33 by rponsonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ int	print_status(t_philo *philo, const char *msg)
 	int	time;
 
 	pthread_mutex_lock(&philo->info->check_dead);
+	pthread_mutex_lock(&philo->info->display);
 	if (philo->info->isdead)
 	{
+		pthread_mutex_unlock(&philo->info->display);
 		pthread_mutex_unlock(&philo->info->check_dead);
 		return (-1);
 	}
 	pthread_mutex_unlock(&philo->info->check_dead);
-	pthread_mutex_lock(&philo->info->display);
 	time = (get_time() - philo->info->time);
 	write(1, "[", 1);
 	ft_putnbr(time);
